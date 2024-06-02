d386e831595e80f060b023cc91ca423f03be2484
gittyupnow <endotactical@gmail.com>
2024-06-02 08:28:24 -0700

    aaa

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-<html>
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
-</html>
-
-
   // <!-- Initialize Swiper -->
   // <!-- https://swiperjs.com/swiper-api#initialize-swiper -->
 
